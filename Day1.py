4416fe51f0e74f7135f104d3ffa72220054c91a5
JeevanPoudel1 <poudelj292@gmail.com>
2026-08-28 15:18:51 +0545

    Adding data's in Github for day1

---
+++ Day1.py
@@ -0,0 +1,8 @@
+name = "Jeevan"
+age = 23
+Studied = "Bachelor"
+print("My name is :", name)
+print("My age is :", age)
+print("I have studied :", Studied)
+print(type(name))
+print(type(age))   
---
+++ Hello.py
@@ -0,0 +1,3 @@
+print("Hello world!")
+print("learning to add in github")
+
