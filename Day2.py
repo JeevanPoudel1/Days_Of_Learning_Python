8c3dbed15e511e6357741c76eba383792f5a8210
JeevanPoudel1 <poudelj292@gmail.com>
2026-08-29 07:28:09 +0545

    adding day 2

---
+++ Day2.py
@@ -0,0 +1,23 @@
+one = 1
+two = 2
+three = 3
+print (three)
+
+hello = "hello"
+world = "world"
+helloworld = hello + " " + world
+print(helloworld)
+
+a,b = 10, 20
+print(a, b)
+
+mystring = "hello"
+myfloat = 10.0
+Myint = 20
+
+if mystring == "hello":
+    print("String: %s" % mystring)
+if isinstance(myfloat, float) and myfloat == 10.0:
+    print("Float: %f" % myfloat)
+if isinstance(Myint, int) and Myint == 20:
+    print("Integer: %d" % Myint)
