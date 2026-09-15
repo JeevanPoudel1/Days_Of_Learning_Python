16b34aeab4954bbaaa7c04ee3e7d631483a8c4d3
JeevanPoudel1 <poudelj292@gmail.com>
2026-09-15 12:54:07 +0545

    numpy

---
+++ day18.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+numbers = np.array([10, 20, 30, 40, 50])
+
+print(numbers)
+print(type(numbers))
+
+
+
+print(np.number)
+print(np.number.shape)
+print(np.number.ndim)
+print(np.number.size)
+print(np.number.dtype)
+
+a=np.zeros(5)
+print(a)
